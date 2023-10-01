aed2b5388295a80df7c06a43680e15e4ca45b387
tjwjddls930 <jungin09307@gmail.com>
2023-10-01 16:20:46 +0900

    artist page fix 8

--- components/exhibits/artistselect/Artistcontent.jsx
+++ components/exhibits/artistselect/Artistcontent.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Artistcontent({order, title, name, text}) {
     return (
         <div className="2xl:w-full flex flex-row 2xl:mx-auto">
-            <span className="text-8xl w-1/12 font-bold text-black pl-32 2xl:text-9xl 2xl:pl-40">{order}</span>
+            <span className="text-8xl w-1/12 2xl:w-2/12 font-bold text-black pl-32 2xl:text-9xl 2xl:pl-56">{order}</span>
             <div className="flex flex-row h-[180px] 2xl:h-[500px] w-1/2 ml-64 2xl:ml-96 items-center shadow-lg px-8 bg-Awhite mx-auto rounded-lg">
                 <div className="flex flex-col space-y-3 2xl:space-y-6 w-1/4">
                     <div className="bg-Cpurple h-[1.5px] w-10 2xl:w-14" />
