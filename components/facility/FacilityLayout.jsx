1b168d49ec70bdf66c7c4e5b11c16fb67af99564
tjwjddls930 <jungin09307@gmail.com>
2023-10-03 16:46:19 +0900

    facility exhibit page fix

--- components/facility/FacilityLayout.jsx
+++ components/facility/FacilityLayout.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function FacilityLayout({children}) {
     return( 
         <Pageframe>
-             <div className="h-screen w-screen bg-[url('/img/facilityguide/exhibitguide/exhibitguide_bg.png')]">
+             <div className="h-screen w-screen bg-[url('/img/facilityguide/exhibitguide/exhibitguide_bg.png')] bg-no-repeat bg-cover">
                 <div className="w-full py-10 px-16 flex flex-row justify-between">
                     <div className="flex flex-row">
                         <Image 
@@ -23,7 +23,7 @@ export default function FacilityLayout({children}) {
                         <span className="text-Agrey text-xl font-bold mt-2">{'> 관람 안내'}</span>
                     </div>
                     <div>
-                        <span className="text-Cgrey textlg font-bold">모두를 위한 보편적인 스마트 디지털 전시 관람 시스템</span>
+                        <span className="text-Cgrey text-lg 2xl:text-xl font-bold">모두를 위한 보편적인 스마트 디지털 전시 관람 시스템</span>
                     </div>
                 </div>
                 {children}
--- pages/facility/exhibitguide/index.js
+++ pages/facility/exhibitguide/index.js
@@ -4,39 +4,39 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 export default function Exhibitguide() {
     return(
         <FacilityLayout>
-             <div className="h-28 w-10/12 flex flex-col bg-Awhite mx-auto items-center space-y-2 p-4">
-                    <div className="bg-Cpurple h-[2.5px] w-[60px] rounded-full"></div>
-                    <span className="text-xl text-Cgrey font-bold">Exhibition guide</span>
-                    <span className="text-2xl text-black font-bold">전시관람 안내</span>
-                </div>
-                <div className="h-[57%] w-10/12 flex flex-row space-x-6 mx-auto justify-center items-center py-14 px-9 bg-Dgrey bg-opacity-50">
+            <div className="h-28 w-10/12 2xl:h-36 flex flex-col bg-Awhite mx-auto items-center space-y-2 p-4 2xl:space-y-4 2xl:p-6">
+                <div className="bg-Cpurple h-[2.5px] w-[60px] 2xl:w-[100px] rounded-full"></div>
+                <span className="text-xl text-Cgrey font-bold 2xl:text-2xl">Exhibition guide</span>
+                <span className="text-2xl text-black font-bold 2xl:text-3xl">전시관람 안내</span>
+            </div>
+                <div className="h-[57%] w-10/12 flex flex-row space-x-6 mx-auto justify-center items-center py-14 px-9 bg-Dgrey bg-opacity-50 2xl:h-[70%] 2xl:space-y-12 2xl:py-20 2xl:px-12">
                     <div className="h-full flex flex-col">
-                        <span className="text-black text-xl font-bold mb-2">관람시간</span>
-                        <span className="text-Cpurple text-md font-bold mb-1">{'1,2월 동절기 | '}<span className="text-black text-md font-bold">오전 10시 ~ 오후 5시</span><span className="text-Cgrey text-md font-bold">{'(오후 4시 30분 입장마감)'}</span></span>
-                        <span className="text-black text-md font-bold mb-5">매주 일요일, 월요일 휴관</span>
-                        <span className="text-Cpurple text-md font-bold mb-1">{'3 ~ 5월/10월 ~ 12월 | '}<span className="text-black text-md font-bold">오전 9시 30분 ~ 오후 5시 30분</span><span className="text-Cgrey text-md font-bold">{'(오후 5시 입장마감)'}</span></span>
-                        <span className="text-black text-md font-bold mb-5">매주 월요일 휴관</span>
-                        <span className="text-Cpurple text-md font-bold mb-1">{'6월 ~ 9월 | '}<span className="text-black text-md font-bold">오전 10시 ~ 오후 6시</span><span className="text-Cgrey text-md font-bold">{'(오후 5시 30분 입장마감)'}</span></span>
-                        <span className="text-black text-md font-bold mb-8">매주 월요일 휴관</span>
-                        <span className="text-black text-xl font-bold mb-3">도슨트 안내</span>
-                        <span className="text-Bgrey text-md font-bold mb-1">도슨트를 원하시는 관람객께서는 사전에 미술관으로 연락주시기 바랍니다.</span>
-                        <span className="text-Cgrey text-sm font-bold mb-4">*미술관의 상황에 따라 진행이 어려울 수도 있습니다.</span>
-                        <div className="text-lg font-bold text-black h-10 w-[250px] bg-Awhite rounded-full text-center pt-1">{'문의 043-221-3269'}</div>
+                        <span className="text-black text-xl font-bold mb-2 2xl:text-2xl 2xl:mb-4">관람시간</span>
+                        <span className="text-Cpurple text-md font-bold mb-1 2xl:text-lg 2xl:mb-2">{'1,2월 동절기 | '}<span className="text-black text-md font-bold">오전 10시 ~ 오후 5시</span><span className="text-Cgrey text-md font-bold">{'(오후 4시 30분 입장마감)'}</span></span>
+                        <span className="text-black text-md font-bold mb-5 2xl:text-lg 2xl:mb-6">매주 일요일, 월요일 휴관</span>
+                        <span className="text-Cpurple text-md font-bold mb-1 2xl:text-lg 2xl:mb-2">{'3 ~ 5월/10월 ~ 12월 | '}<span className="text-black text-md font-bold">오전 9시 30분 ~ 오후 5시 30분</span><span className="text-Cgrey text-md font-bold">{'(오후 5시 입장마감)'}</span></span>
+                        <span className="text-black text-md font-bold mb-5 2xl:text-lg 2xl:mb-6">매주 월요일 휴관</span>
+                        <span className="text-Cpurple text-md font-bold mb-1 2xl:text-lg 2xl:mb-2">{'6월 ~ 9월 | '}<span className="text-black text-md font-bold">오전 10시 ~ 오후 6시</span><span className="text-Cgrey text-md font-bold">{'(오후 5시 30분 입장마감)'}</span></span>
+                        <span className="text-black text-md font-bold mb-8 2xl:text-lg 2xl:mb-9">매주 월요일 휴관</span>
+                        <span className="text-black text-xl font-bold mb-3 2xl:text-2xl 2xl:mb-4">도슨트 안내</span>
+                        <span className="text-Bgrey text-md font-bold mb-1 2xl:text-lg 2xl:mb-2">도슨트를 원하시는 관람객께서는 사전에 미술관으로 연락주시기 바랍니다.</span>
+                        <span className="text-Cgrey text-sm font-bold mb-4 2xl:text-base 2xl:mb-5">*미술관의 상황에 따라 진행이 어려울 수도 있습니다.</span>
+                        <div className="text-lg font-bold text-black h-10 w-[250px] 2xl:text-xl 2xl:h-12 2xl:w-[350px] bg-Awhite rounded-full text-center pt-1 2xl:pt-2">{'문의 043-221-3269'}</div>
                     </div>
                     <div className="h-full flex flex-col">
-                        <span className="text-black text-xl font-bold mb-3">관람료</span>
-                        <span className="text-Cpurple text-md font-bold mb-1">{'성인 (19세 이상 64세 이하)'}<span className="text-Cgrey text-md font-bold">{' | '}</span><span className="text-black text-md font-bold">{'2,000원'}</span></span>
-                        <span className="text-Cpurple text-md font-bold mb-1">{'청소년 (중, 고등학생 포함 13세 이상 18세 이하)'}<span className="text-Cgrey text-md font-bold">{' | '}</span><span className="text-black text-md font-bold">{'1,000원'}</span></span>
-                        <span className="text-Cpurple text-md font-bold mb-1">{'어린이 (초등학생 포함 7세 이상 12세 이하'}<span className="text-Cgrey text-md font-bold">{' | '}</span><span className="text-black text-md font-bold">{'1,000원'}</span></span>
-                        <span className="text-Cgrey text-sm font-bold mb-6">* 기획전 또는 특별전의 경우 별도의 관람료가 책정될 수 있습니다.</span>
+                        <span className="text-black text-xl font-bold mb-3 2xl:text-2xl 2xl:mb-4">관람료</span>
+                        <span className="text-Cpurple text-md font-bold mb-1 2xl:text-lg 2xl:mb-2">{'성인 (19세 이상 64세 이하)'}<span className="text-Cgrey text-md font-bold">{' | '}</span><span className="text-black text-md font-bold">{'2,000원'}</span></span>
+                        <span className="text-Cpurple text-md font-bold mb-1 2xl:text-lg 2xl:mb-2">{'청소년 (중, 고등학생 포함 13세 이상 18세 이하)'}<span className="text-Cgrey text-md font-bold">{' | '}</span><span className="text-black text-md font-bold">{'1,000원'}</span></span>
+                        <span className="text-Cpurple text-md font-bold mb-1 2xl:text-lg 2xl:mb-2">{'어린이 (초등학생 포함 7세 이상 12세 이하'}<span className="text-Cgrey text-md font-bold">{' | '}</span><span className="text-black text-md font-bold">{'1,000원'}</span></span>
+                        <span className="text-Cgrey text-sm font-bold mb-6 2xl:text-base 2xl:mb-7">* 기획전 또는 특별전의 경우 별도의 관람료가 책정될 수 있습니다.</span>
 
-                        <span className="text-black text-xl font-bold mb-1">할인대상</span>
-                        <span className="text-Bgrey text-md font-bold mb-6">{'단체 5인 이상 50% (방문 7일 전 예약 단체에 한함)'}</span>
-                        <span className="text-black text-xl font-bold mb-1">무료대상</span>
-                        <span className="text-Bgrey text-md font-bold mb-1">{'- 6세 이하 어린이, 65세 이상 어르신'}</span>
-                        <span className="text-Bgrey text-md font-bold mb-1">{'- 장애인 복지법에 따른 장애인'}</span>
-                        <span className="text-Bgrey text-md font-bold mb-1">{'- 국가유공자 등예우 및 지원에 관한 법률에 따른 국가 유공자와 유족'}</span>
-                        <span className="text-Cgrey text-sm font-bold mb-1">{' (신분증 또는 증명서류 제시자에 한함)'}</span>
+                        <span className="text-black text-xl font-bold mb-1 2xl:text-2xl 2xl:mb-2">할인대상</span>
+                        <span className="text-Bgrey text-md font-bold mb-6 2xl:text-lg 2xl:mb-7">{'단체 5인 이상 50% (방문 7일 전 예약 단체에 한함)'}</span>
+                        <span className="text-black text-xl font-bold mb-1 2xl:text-2xl 2xl:mb-2">무료대상</span>
+                        <span className="text-Bgrey text-md font-bold mb-1 2xl:text-lg 2xl:mb-2">{'- 6세 이하 어린이, 65세 이상 어르신'}</span>
+                        <span className="text-Bgrey text-md font-bold mb-1 2xl:text-lg 2xl:mb-2">{'- 장애인 복지법에 따른 장애인'}</span>
+                        <span className="text-Bgrey text-md font-bold mb-1 2xl:text-lg 2xl:mb-2">{'- 국가유공자 등예우 및 지원에 관한 법률에 따른 국가 유공자와 유족'}</span>
+                        <span className="text-Cgrey text-sm font-bold mb-1 2xl:text-lg 2xl:mb-2">{' (신분증 또는 증명서류 제시자에 한함)'}</span>
                     </div>
                 </div>
         </FacilityLayout>
