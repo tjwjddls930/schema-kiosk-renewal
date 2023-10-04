79d2cbbdec80e0ac0cf935260c1b1d754358be2b
tjwjddls930 <jungin09307@gmail.com>
2023-10-04 16:05:08 +0900

    web popup update5

--- components/connectingpage/Webpopup.jsx
+++ components/connectingpage/Webpopup.jsx
@@ -19,9 +19,10 @@ export default function Webpopup() {
                     position={[0, 1.56, -1.4]}
                     // rotation-x={-0.256}
                 >
+                  <iframe>
                     <Link href="https://schemaartmuseum.com/2023/09/09/%eb%a7%a4%eb%81%84%eb%9f%ac%ec%9a%b4-%eb%8f%8c%eb%b0%ad-2-2/">
-                        <iframe src="https://schemaartmuseum.com/2023/09/09/%eb%a7%a4%eb%81%84%eb%9f%ac%ec%9a%b4-%eb%8f%8c%eb%b0%ad-2-2/"/>
                     </Link>
+                  </iframe>
                 </Html>
             </Canvas>
         </div>
