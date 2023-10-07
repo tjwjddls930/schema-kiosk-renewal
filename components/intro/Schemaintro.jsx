ffd49c10892362968ac91d57bfebc20e2d45f5ed
tjwjddls930 <jungin09307@gmail.com>
2023-10-07 16:15:42 +0900

    intro structure page fix2

--- components/intro/Schemaintro.jsx
+++ components/intro/Schemaintro.jsx
@@ -20,7 +20,7 @@ export default function Schemaintro() {
                     /> */}
                     <img 
                         src="/img/intro/introduction/schema_small.jpg"
-                        className="h-[250px] w-[150px] mx-auto 2xl:h-[600px] 2xl:w-[200px]"
+                        className="h-[250px] w-[150px] mx-auto 2xl:h-[600px] 2xl:w-[300px]"
                         alt="inside"
                     />
                 </div>
@@ -29,48 +29,48 @@ export default function Schemaintro() {
             <div className="h-5/6 w-2/5 flex flex-col space-y-4">
                 <div className="flex flex-col space-y-2">
                     <div className="h-[2px] w-14 bg-Cpurple"></div>
-                    <span className="font-bold text-lg 2xl:text-3xl text-Cgrey">Schema Art Museum</span>
-                    <span className="font-bold text-xl 2xl:text-5xl text-black">쉐마미술관</span>
+                    <span className="font-bold text-lg 2xl:text-5xl text-Cgrey">Schema Art Museum</span>
+                    <span className="font-bold text-xl 2xl:text-7xl text-black">쉐마미술관</span>
                 </div>
-                <p className="text-Cgrey text-sm 2xl:text-xl font-bold">
+                <p className="text-Cgrey text-sm 2xl:text-3xl font-bold">
                     {'쉐마미술관은 그동안 대학 강단에서 30여 년간 교수로서의 길을 걸었던 서양화가 김재관 교수가 예술과 교육자로서의 열정과 지역 예술인들과 지역민들의 예술적 교감의 필요성을 인지하고, 고향 청주 근교에 2009년 6월 26일 설립한 사설 미술관으로써'}
-                    <span className="text-Cpurple text-sm 2xl:text-xl">{'충청북도 제 2011-01호 제1종 미술관'}</span> {'이며, 최근 한국박물관협회, 한국과학창의재단, 교육과학기술부로부터'}
-                     <span className="text-Cpurple text-sm 2xl:text-xl">{'박물'}</span>
-                    <span className="text-Cpurple text-sm 2xl:text-xl">{'관, 미술관 창제 프로그램 우수 운영기관으로 선정'}</span>{'되었습니다.'}
+                    <span className="text-Cpurple text-sm 2xl:text-3xl">{'충청북도 제 2011-01호 제1종 미술관'}</span> {'이며, 최근 한국박물관협회, 한국과학창의재단, 교육과학기술부로부터'}
+                     <span className="text-Cpurple text-sm 2xl:text-3xl">{'박물'}</span>
+                    <span className="text-Cpurple text-sm 2xl:text-3xl">{'관, 미술관 창제 프로그램 우수 운영기관으로 선정'}</span>{'되었습니다.'}
                 </p>
                 <div className="mx-auto text-center flex flex-row space-x-6">
-                    <div className="pt-7 h-20 w-20 2xl:h-32 2xl:w-32 shadow-2xl rounded-full font-bold text-black text-base 2xl:text-2xl z-10">
+                    <div className="pt-7 2xl:pt-12 h-20 w-20 2xl:h-40 2xl:w-40 shadow-2xl rounded-full font-bold text-black text-base 2xl:text-4xl z-10">
                         격
                     </div>
-                    <div className="pt-7 h-20 w-20 2xl:h-32 2xl:w-32 shadow-2xl rounded-full font-bold text-black text-base 2xl:text-2xl z-10">
+                    <div className="pt-7 2xl:pt-12 h-20 w-20 2xl:h-40 2xl:w-40 shadow-2xl rounded-full font-bold text-black text-base 2xl:text-4xl z-10">
                         짜임
                     </div>
-                    <div className="pt-7 h-20 w-20 2xl:h-32 2xl:w-32 shadow-2xl rounded-full font-bold text-black text-base 2xl:text-2xl z-10">
+                    <div className="pt-7 2xl:pt-12 h-20 w-20 2xl:h-40 2xl:w-40 shadow-2xl rounded-full font-bold text-black text-base 2xl:text-4xl z-10">
                         계획
                     </div>
                 </div>
 
-                <p className="text-Cgrey text-sm 2xl:text-xl font-bold">
+                <p className="text-Cgrey text-sm 2xl:text-3xl font-bold">
                     {'SCHEMA는 "격", "짜임", "계획"이라는 의미로 기하학적 구성요소로 이루어진 김재관 교수의 작품과 그 맥을 같이 하며, 미학적, 종교적 의미로서의 SCHEMA는 "하나님께 모든 것을 바친다."는 의미가 있습니다. 이는 미술관의 지역사회, 미술계에 봉사하고자 하는 열망으로 청주지역 뿐만 아니라'} 
-                    <span className="text-Cpurple text-sm 2xl:text-xl">{'현대미술'}</span>
-                    <span className="text-Cpurple text-sm 2xl:text-xl">{'의 격이 시대정신에 걸 맞는 창작 예술의 연구와 실천을 위한'}</span>
-                    <span className="text-Cpurple text-sm 2xl:text-xl">{'공간'}</span>{'이 될 수 있도록 최선의 역할을 하고자 합니다.'}
+                    <span className="text-Cpurple text-sm 2xl:text-3xl">{'현대미술'}</span>
+                    <span className="text-Cpurple text-sm 2xl:text-3xl">{'의 격이 시대정신에 걸 맞는 창작 예술의 연구와 실천을 위한'}</span>
+                    <span className="text-Cpurple text-sm 2xl:text-3xl">{'공간'}</span>{'이 될 수 있도록 최선의 역할을 하고자 합니다.'}
                 </p>
             </div>
             <div className="h-1/2 w-2/3 flex flex-row space-x-8">
-                <p className="text-Cgrey text-sm 2xl:text-xl font-bold">
+                <p className="text-Cgrey text-sm 2xl:text-3xl font-bold">
                     {'특히 이 지역 젊은 작가들에게 다양하고 새로운 현대미술의 정보를 제공하고 국내,외 현대미술의 창의적인 작품들로 구성된 기획전시를 통하여'}
-                    <span className="text-Cpurple text-sm 2xl:text-xl">{'지역 국제 교류센터'}</span>
+                    <span className="text-Cpurple text-sm 2xl:text-3xl">{'지역 국제 교류센터'}</span>
                     {'로서의 역할을 하고자하며 예술가 뿐만 아니라 문화시민을 위해 제2의 교육기관으로 지역 사회단체의 문화예술교육 프로젝트뿐만 아니라 유소년, 청소냔, 어린이 등 '}
-                    <span className="text-Cpurple text-sm 2xl:text-xl">{'다양한 계층의 다양한 문화예술체험 교'}</span>
-                    <span className="text-Cpurple text-sm 2xl:text-xl">{'육기관'}</span>{'으로의 기능을 수행하고자 합니다.'}
+                    <span className="text-Cpurple text-sm 2xl:text-3xl">{'다양한 계층의 다양한 문화예술체험 교'}</span>
+                    <span className="text-Cpurple text-sm 2xl:text-3xl">{'육기관'}</span>{'으로의 기능을 수행하고자 합니다.'}
                 </p>
-                <p className="text-Cgrey text-sm 2xl:text-xl font-bold">
+                <p className="text-Cgrey text-sm 2xl:text-3xl font-bold">
                     {'향후 예술가뿐만 아니라 문화시민을 위한 사회교육, 어린이 예술체험교육의 다양한 프로그램 개발과 독창적인 전시기획으로 내실을 다지고, 지역사회와 함께하는 사업을 추진하는 공공 미술관으로서의 역할을 하고자 합니다.'}
                 </p>
             </div>
 
-            <div className="absolute right-64 bottom-56 2xl:right-80 2xl:bottom-80">
+            <div className="absolute right-64 bottom-56 2xl:right-96 2xl:bottom-96">
                 <div className="h-[100px] w-[400px] 2xl:h-[200px] 2xl:w-[600px] flex flex-row space-x-4 mx-auto">
                     <div className="h-2 w-2 2xl:h-4 2xl:w-4 bg-Cpurple rounded-full"></div>
                     <div className="h-2 w-2 2xl:h-4 2xl:w-4 bg-Cpurple rounded-full"></div>
@@ -78,7 +78,7 @@ export default function Schemaintro() {
                 </div>
             </div>
 
-            <div className="absolute right-4 bottom-64 2xl:right-8 2xl:bottom-80">
+            <div className="absolute right-4 bottom-64 2xl:right-8 2xl:bottom-96">
                 <img
                     className="h-[80px] w-[480px] 2xl:h-[200px] 2xl:w-[850px]"
                     src="/img/intro/introduction/schema_art_museum_typo.png"
@@ -88,7 +88,7 @@ export default function Schemaintro() {
 
             <div className="absolute right-0 bottom-0 2xl:bottom-10">
                 <img 
-                    className="h-[250px] w-[820px] 2xl:h-1/4 2xl:w-1/2" 
+                    className="h-[250px] w-[820px] 2xl:h-[450px] 2xl:w-[1200px]" 
                     src="/img/intro/introduction/schema_image_보정.png"
                     alt="schema_image_2"
                 />
