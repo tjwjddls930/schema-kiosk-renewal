7d7cf118d9572087e2a183b427c68c74b1cbb25c
tjwjddls930 <jungin09307@gmail.com>
2023-10-07 12:18:19 +0900

    main button fix2

--- pages/main/index.js
+++ pages/main/index.js
@@ -47,22 +47,26 @@ export default function Mainpage() {
                     <div className="w-1/3 flex flex-col space-y-5 text-Agrey shadow-md rounded-sm">
                         <button className="w-full h-1/2 text-center bg-[url('/img/mainpage/미술관소개_보정이미지.png')] bg-no-repeat bg-cover rounded-lg bg-opacity-80">
                             <Link href="/intro">
-                                <div className="flex flex-col space-y-2 bg-[url('/img/mainpage/미술관소개_도형.png')] bg-no-repeat bg-cover">
-                                    <span className={`font-bold text-Awhite ${size ? "text-6xl" : "text-lg 2xl:text-4xl"}`}>{t("MAIN_INTRO")}</span>
-                                    <span className={`font-bold text-Awhite ${size ? "text-4xl" : "text-sm 2xl:text-2xl"}`}>{t("MAIN_INTRO_1")}</span>
-                                    <span className={`font-bold text-Awhite ${size ? "text-4xl" : "text-sm 2xl:text-2xl"}`}>{t("MAIN_INTRO_2")}</span>
+                                <div className="h-3/4 w-3/4 bg-[url('/img/mainpage/미술관소개_도형.png')] bg-no-repeat bg-cover mx-auto">
+                                    <div className="flex flex-col space-y-2">
+                                        <span className={`font-bold text-Awhite ${size ? "text-6xl" : "text-lg 2xl:text-4xl"}`}>{t("MAIN_INTRO")}</span>
+                                        <span className={`font-bold text-Awhite ${size ? "text-4xl" : "text-sm 2xl:text-2xl"}`}>{t("MAIN_INTRO_1")}</span>
+                                        <span className={`font-bold text-Awhite ${size ? "text-4xl" : "text-sm 2xl:text-2xl"}`}>{t("MAIN_INTRO_2")}</span>
+                                    </div>
                                 </div>
                             </Link>
                         </button>
-                        <button className="w-full h-1/2 text-center bg-Bpurple text-white shadow-md rounded-lg">
-                                <div className="flex flex-col space-y-2">
-                                    <span className="text-lg 2xl:text-4xl font-bold">{t("MAIN_CHATBOT")}</span>
-                                    <span className="text-sm 2xl:text-2xl font-bold">{t("MAIN_CHATBOT_1")}</span>
+                        <button className="w-full h-1/2 text-center text-white shadow-md rounded-lg bg-[url('/img/mainpage/챗봇안내_보정이미지.png')] bg-cover bg-no-repeat">
+                                <div className="h-3/4 w-3/4 mx-auto bg-[url('/img/mainpage/챗봇안내_도형.png')] bg-cover bg-no-repeat">
+                                    <div className="flex flex-col space-y-2">
+                                        <span className="text-lg 2xl:text-4xl font-bold">{t("MAIN_CHATBOT")}</span>
+                                        <span className="text-sm 2xl:text-2xl font-bold">{t("MAIN_CHATBOT_1")}</span>
+                                    </div>
                                 </div>
                         </button>
                     </div>
                     <div className="flex w-full h-full bg-[url('/img/mainpage/전시안내_보정이미지.png')] items-center shadow-md rounded-lg bg-cover bg-no-repeat">
-                        <button className="w-3/4 h-3/4 bg-[url('/img/mainpage/전시안내_도형.png)] bg-cover bg-no-repeat mx-auto">
+                        <button className="w-3/4 h-3/4 bg-[url('/img/mainpage/전시안내_도형.png')] bg-cover bg-no-repeat mx-auto">
                             <Link href="/exhibit">
                                 <div className="flex flex-col space-y-2">
                                     <span className="text-xl 2xl:text-4xl font-bold text-Awhite">{t("MAIN_EXHIBIT_GUIDE")}</span>
@@ -73,16 +77,20 @@ export default function Mainpage() {
                     </div>
                     <div className="w-1/3 flex flex-col space-y-5 text-Awhite shadow-md">
                         <button className="w-full h-1/2 text-center bg-[url('/img/mainpage/교육프로그램_보정이미지.png')] bg-no-repeat bg-cover rounded-lg">
-                            <div className="flex flex-col space-y-2 bg-[url('/img/mainpage/교육프로그램_도형.png')] bg-no-repeat bg-cover">
-                                <span className="text-lg 2xl:text-4xl font-bold">{t("MAIN_EDUCATION")}</span>
-                                <span className="text-sm 2xl:text-2xl font-bold">{t("MAIN_EDUCATION_1")}</span>
+                            <div className="h-3/4 w-3/4 mx-auto bg-[url('/img/mainpage/교육프로그램_도형.png')] bg-no-repeat bg-cover">
+                                <div className="flex flex-col space-y-2">
+                                    <span className="text-lg 2xl:text-4xl font-bold">{t("MAIN_EDUCATION")}</span>
+                                    <span className="text-sm 2xl:text-2xl font-bold">{t("MAIN_EDUCATION_1")}</span>
+                                </div>
                             </div>
                         </button>
                         <button className="w-full h-1/2 text-center bg-[url('/img/mainpage/시설안내_보정이미지.png')] bg-no-repeat bg-cover text-white shadow-md rounded-lg">
                             <Link href="https://schema-kiosk.vercel.app/floorguide">
-                                <div className="flex flex-col space-y-2 bg-[url('/img/mainpage/시설안내_도형.png')] bg-no-repeat bg-cover">
-                                    <span className="text-lg 2xl:text-4xl font-bold">{t("MAIN_FACILITY_GUIDE")}</span>
-                                    <span className="text-sm 2xl:text-2xl font-bold">{t("MAIN_FACILITY_GUIDE_1")}</span>
+                                <div className="h-3/4 w-3/4 mx-auto bg-[url('/img/mainpage/시설안내_도형.png')] bg-no-repeat bg-cover">
+                                    <div className="flex flex-col space-y-2">
+                                        <span className="text-lg 2xl:text-4xl font-bold">{t("MAIN_FACILITY_GUIDE")}</span>
+                                        <span className="text-sm 2xl:text-2xl font-bold">{t("MAIN_FACILITY_GUIDE_1")}</span>
+                                    </div>
                                 </div>
                             </Link>
                         </button>
