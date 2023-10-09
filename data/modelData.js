b152ec425099ff7fa579bf6b6a7a7ee0b9c012cb
tjwjddls930 <jungin09307@gmail.com>
2023-10-09 19:03:55 +0900

    model page update

--- data/modelData.js
+++ data/modelData.js
@@ -1,10 +1,10 @@
 
 
 export const modelData = [
-    {order: 0, title: "조형물1", name: "", url: "/models/schemalist_2.glb", scale: [0.6, 0.6, 0.6], overview: "", overview1: "",},
+    {order: 0, title: "조형물1", name: "", url: "/models/schema_model_14.glb", scale: [1, 1, 1], overview: "", overview1: "",},
     {order: 1, title: "조형물2", name: "", url: "/models/schema_model_2.glb", scale: [0.6, 0.6, 0.6], overview: "", overview1: "",},
-    {order: 2, title: "조형물3", name: "", url: "/models/schema_model_7.glb", scale: [1, 1, 1], overview: "", overview1: "",},
+    {order: 2, title: "조형물3", name: "", url: "/models/schema_model_7_1.glb", scale: [1, 1, 1], overview: "", overview1: "",},
     {order: 3, title: "조형물4", name: "", url: "/models/schemalist_1.glb", scale: [1, 1, 1], overview: "", overview1: "",},
-    // {order: 4, title: "조형물5", name: "", url: "/models/schemalist_2.glb", overview: "", overview1: "",},
+    // {order: 4, title: "조형물5", name: "", url: "/models/schemalist_2.glb",overview: "", overview1: "",},
 
 ]
