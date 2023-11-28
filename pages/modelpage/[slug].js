58b8fb7f99da9071aab493fcb2f3ee7a20d45cc7
tjwjddls930 <jungin09307@gmail.com>
2023-11-29 02:17:31 +0900

    modelpage code clean

--- pages/modelpage/[slug].js
+++ pages/modelpage/[slug].js
@@ -121,12 +121,6 @@ export default function Modelpage({}) {
                                 shadows
                             >
                                 <Suspense fallback={null}>
-                                    {/* <Modelanimation 
-                                        modelName={data?.url}
-                                        position={data?.position}
-                                        rotation={[0, 0, 0]}
-                                        scale={data?.scale}
-                                    /> */}
                                     <IDBAnimationModel 
                                         keyName={data.keyname}
                                         modelSrc={data.url}
@@ -269,11 +263,6 @@ export default function Modelpage({}) {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                 </svg>
             </button>
-              {/* <ViewNavbar 
-            url={"/video/docent/blue-docent-test-02-removed.webm"}
-            lang={"viewpage"}
-            pid={mid}
-            /> */}
             <Navbar 
                 url={"/video/docent/schema-docent-04.webm"}
                 sign={"/video/sign/schema_sign_2.mp4"}
