4b09c61bd580a8b704e38b0f16ad2c57c91bcdf5
tjwjddls930 <jungin09307@gmail.com>
2024-01-02 11:56:49 +0900

    data: new model data update

--- data/modelData.js
+++ data/modelData.js
@@ -11,6 +11,8 @@ export const modelData_KOR = [
     {order: 8, title: "조형물9", name: "", keyname: "schema_model_6_2.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_6_2.glb`, position:[0, 0, 0], scale: [5, 5, 5], overview: "", overview1: "",},
     {order: 9, title: "조형물10", name: "", keyname: "schema_model_7.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_7.glb`, position:[0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 10, title: "조형물11", name: "", keyname: "schema_model_10.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_10.glb`, position:[0, -1.5, 0], scale: [2, 2, 2], overview: "", overview1: "",},
+    {order: 11, title: "조형물12", name: "", keyname: "schema_model_11.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_11.glb`, position:[0, -1.5, 0], scale: [1.5, 1.5, 1.5], overview: "", overview1: "",},
+    {order: 12, title: "조형물13", name: "", keyname: "schema_model_13.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_13.glb`, position:[0, -1, 0], scale: [3.5, 3.5, 3.5], overview: "", overview1: "",},
 ];
 
 export const modelData_ENG = [
@@ -25,6 +27,8 @@ export const modelData_ENG = [
     {order: 8, title: "Sculpture 9", name: "", keyname: "schema_model_6_2.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_6_2.glb`, position:[0, 0, 0], scale: [5, 5, 5], overview: "", overview1: "",},
     {order: 9, title: "Sculpture 10", name: "", keyname: "schema_model_7.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_7.glb`, position:[0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 10, title: "Sculpture 11", name: "", keyname: "schema_model_10.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_10.glb`, position:[0, -1.5, 0], scale: [2, 2, 2], overview: "", overview1: "",},
+    {order: 11, title: "Sculpture 12", name: "", keyname: "schema_model_11.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_11.glb`, position:[0, -1.5, 0], scale: [1.5, 1.5, 1.5], overview: "", overview1: "",},
+    {order: 12, title: "Sculpture 13", name: "", keyname: "schema_model_13.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_13.glb`, position:[0, -1, 0], scale: [3.5, 3.5, 3.5], overview: "", overview1: "",},
 ];
 
 export const modelData_CH = [
@@ -39,6 +43,8 @@ export const modelData_CH = [
     {order: 8, title: "雕塑9", name: "", keyname: "schema_model_6_2.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_6_2.glb`, position:[0, 0, 0], scale: [5, 5, 5], overview: "", overview1: "",},
     {order: 9, title: "雕塑10", name: "", keyname: "schema_model_7.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_7.glb`, position:[0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 10, title: "雕塑11", name: "", keyname: "schema_model_10.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_10.glb`, position:[0, -1.5, 0], scale: [2, 2, 2], overview: "", overview1: "",},
+    {order: 11, title: "雕塑12", name: "", keyname: "schema_model_11.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_11.glb`, position:[0, -1.5, 0], scale: [1.5, 1.5, 1.5], overview: "", overview1: "",},
+    {order: 12, title: "雕塑13", name: "", keyname: "schema_model_13.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_13.glb`, position:[0, -1, 0], scale: [3.5, 3.5, 3.5], overview: "", overview1: "",},
 ];
 
 export const modelData_TH = [
@@ -53,6 +59,8 @@ export const modelData_TH = [
     {order: 8, title: "ประติมากรรม9", name: "", keyname: "schema_model_6_2.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_6_2.glb`, position:[0, 0, 0], scale: [5, 5, 5], overview: "", overview1: "",},
     {order: 9, title: "ประติมากรรม10", name: "", keyname: "schema_model_7.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_7.glb`, position:[0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 10, title: "ประติมากรรม11", name: "", keyname: "schema_model_10.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_10.glb`, position:[0, -1.5, 0], scale: [2, 2, 2], overview: "", overview1: "",},
+    {order: 11, title: "ประติมากรรม12", name: "", keyname: "schema_model_11.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_11.glb`, position:[0, -1.5, 0], scale: [1.5, 1.5, 1.5], overview: "", overview1: "",},
+    {order: 12, title: "ประติมากรรม13", name: "", keyname: "schema_model_13.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_13.glb`, position:[0, -1, 0], scale: [3.5, 3.5, 3.5], overview: "", overview1: "",},
 ];
 
 export const modelData_VI = [
@@ -67,4 +75,6 @@ export const modelData_VI = [
     {order: 8, title: "Điêu khắc9", name: "", keyname: "schema_model_6_2.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_6_2.glb`, position:[0, 0, 0], scale: [5, 5, 5], overview: "", overview1: "",},
     {order: 9, title: "Điêu khắc10", name: "", keyname: "schema_model_7.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_7.glb`, position:[0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 10, title: "Điêu khắc11", name: "", keyname: "schema_model_10.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_10.glb`, position:[0, -1.5, 0], scale: [2, 2, 2], overview: "", overview1: "",},
+    {order: 11, title: "Điêu khắc12", name: "", keyname: "schema_model_11.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_11.glb`, position:[0, -1.5, 0], scale: [1.5, 1.5, 1.5], overview: "", overview1: "",},
+    {order: 12, title: "Điêu khắc13", name: "", keyname: "schema_model_13.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_13.glb`, position:[0, -1, 0], scale: [3.5, 3.5, 3.5], overview: "", overview1: "",},
 ];
