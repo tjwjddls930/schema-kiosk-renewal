b094d7dde9afa25d1d1c0d0ef217495d2e50db5f
tjwjddls930 <jungin09307@gmail.com>
2024-01-16 19:53:08 +0900

    data: modelData mistake fix

--- data/modelData.js
+++ data/modelData.js
@@ -35,7 +35,7 @@ export const modelData_ENG = [
 
 export const modelData_CH = [
     {order: 0, title: "雕塑1", name: "", keyname: "schema_model_14.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_14.glb`, position: [0, -1, 0], scale: [1, 1, 1], overview: "", overview1: "",},
-    {order: 4, title: "雕塑2", name: "", keyname: "schema_model_2_1.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_2_1.glb`, position: [0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
+    {order: 1, title: "雕塑2", name: "", keyname: "schema_model_2_1.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_2_1.glb`, position: [0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 2, title: "永", name: "金润华", keyname: "schema_model_list_2.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_list_2.glb`, position:[0, -1, -1], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 3, title: "雕塑4", name: "", keyname: "schemalist_4(re).glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schemalist_4(re).glb`, position:[0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 4, title: "雕塑5", name: "", keyname: "schema_model_3.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_3.glb`, position:[0, -1, 0], scale: [2, 2, 2], overview: "", overview1: "",},
@@ -69,7 +69,7 @@ export const modelData_TH = [
 
 export const modelData_VI = [
     {order: 0, title: "Điêu khắc1", name: "", keyname: "schema_model_14.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_14.glb`, position: [0, -1, 0], scale: [1, 1, 1], overview: "", overview1: "",},
-    {order: 2, title: "Điêu khắc2", name: "", keyname: "schema_model_2_1.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_2_1.glb`, position: [0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
+    {order: 1, title: "Điêu khắc2", name: "", keyname: "schema_model_2_1.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_2_1.glb`, position: [0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 2, title: "Eon", name: "YoonHwa Kim", keyname: "schema_model_list_2.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_list_2.glb`, position:[0, -1, -1], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 3, title: "Điêu khắc4", name: "", keyname: "schemalist_4(re).glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schemalist_4(re).glb`, position:[0, 0, 0], scale: [4, 4, 4], overview: "", overview1: "",},
     {order: 4, title: "Điêu khắc5", name: "", keyname: "schema_model_3.glb", url: `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_ENDPOINT}/models/schema_model_3.glb`, position:[0, -1, 0], scale: [2, 2, 2], overview: "", overview1: "",},
